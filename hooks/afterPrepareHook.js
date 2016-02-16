5949fba03da2467dbcc95643ee093c7a77714682
Bo Yu <bohr.yu@gmail.com>
2016-02-16 00:45:30 -0800

    added missing parameter in android hooks

--- hooks/afterPrepareHook.js
+++ hooks/afterPrepareHook.js
@@ -54,10 +54,10 @@ function run(cordovaContext) {
  */
 function activateUniversalLinksInAndroid(cordovaContext) {
   // inject preferenes into AndroidManifest.xml
-  androidManifestWriter.writePreferences(cordovaContext);
+  androidManifestWriter.writePreferences(cordovaContext, pluginPreferences);
 
   // generate html file with the <link> tags that you should inject on the website.
-  androidWebHook.generate(cordovaContext);
+  androidWebHook.generate(cordovaContext, pluginPreferences);
 }
 
 /**
